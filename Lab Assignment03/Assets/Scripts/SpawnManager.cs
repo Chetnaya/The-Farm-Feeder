25559d45b51d394b68bf8b8ed17223d8370fabf8
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-01-28 20:03:19 +0530

    Spawned animals at timed intervals

--- Lab Assignment03/Assets/Scripts/SpawnManager.cs
+++ Lab Assignment03/Assets/Scripts/SpawnManager.cs
@@ -11,16 +11,34 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangex = 13;
     private float spawnPosz = 44;
 
-    // Update is called once per frame
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating( "SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+    
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
+        // if(Input.GetKeyDown(KeyCode.S))
+        // {
+        // SpawnRandomAnimal();    
+        // }
+
+        // void SpawnRandomAnimal()
+        // {
+        //     Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex),0, spawnPosz);
+
+        //     int animalIndex = Random.Range(0,animalPrefabs.Length);
+        //     Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        // }
+    }
+        void SpawnRandomAnimal()
         {
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex),0, spawnPosz);
 
             int animalIndex = Random.Range(0,animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-            
         }
-    }
 }
