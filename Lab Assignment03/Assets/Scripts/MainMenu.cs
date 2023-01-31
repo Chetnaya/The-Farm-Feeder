ccf222b0f9349169076eebbfda51b2d8a5a71b60
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-01-31 09:19:47 +0530

    Script to load Level01 in Main Menu

---
+++ Lab Assignment03/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //Loads the Scene 01 on clicking "Pkay in the menu"
+        SceneManager.LoadScene("Scene01");
+    }
+    
+    public void QuitGame()
+    {
+     // save any game data here
+     #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+     #else
+         Application.Quit();
+     #endif
+     
+     }
+}
